492cb272a1e8f26db41230b3f091cbb394468a08
Elkien <zgiorz@gmail.com>
2025-10-22 22:39:42 +0200

    removed completely umbrella-rain

--- src/modules/animations/index.js
+++ src/modules/animations/index.js
@@ -3,20 +3,15 @@
  */
 
 import { TypingAnimation } from './typing.js';
-import { UmbrellaRain, initUmbrellaRain } from './umbrella-rain.js';
 import { initScrollAnimations, initBorderRadiusAnimations } from './scroll-animations.js';
 
 export { TypingAnimation } from './typing.js';
-export { UmbrellaRain, initUmbrellaRain } from './umbrella-rain.js';
 export { initScrollAnimations, initBorderRadiusAnimations } from './scroll-animations.js';
 
 export function initAnimations() {
     // Typing animation
     const typing = new TypingAnimation();
     typing.start();
-
-    // Umbrella rain
-    initUmbrellaRain();
 
     // Scroll-based animations
     initScrollAnimations();
